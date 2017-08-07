42fb738afc8b5307dadb80c9daa560b0edcbfaf5
juliazxu <juliazxu@gmail.com>
2017-08-07 00:34:54 -0700

    finished tutorial

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-    res.sendFile(__dirname + '/index.css');
-    res.sendFile(__dirname + '/client.js');
+    res.sendFile(__dirname + '/index.html')
 });
 
 io.on('connection', function(socket) {
@@ -15,6 +17,18 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3001, function() {
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
